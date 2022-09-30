ab028f50a02cd33dcad669e8f648ae3cd7808af9
bodhi <jyxz5@qq.com>
2022-09-30 13:07:06 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pywiki",
+    version="1.0",
+    packages=find_packages(),
+    # if you don't already have DataHub Actions installed, add it under install_requires
+    # install_requires=["pywiki"]
+)
